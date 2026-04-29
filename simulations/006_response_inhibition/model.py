fbd6dc687cd6ca7f1ca4ca16feb1271c9034d396
soelderer <p.soelder@mailbox.org>
2026-04-29 16:44:00 +0200

    simulation 6

---
+++ simulations/006_response_inhibition/model.py
@@ -0,0 +1,76 @@
+import mesa  # agent-based model package
+import numpy as np
+from typing import Union
+import agents
+import importlib
+importlib.reload(agents)
+IrritabilityAgent = agents.IrritabilityAgent
+
+RealNumber = Union[float, np.floating]
+
+
+class IrritabilityModel(mesa.Model):
+    """A model with some number of agents."""
+
+    def __init__(
+        self,
+        seed: int,
+        V: RealNumber,             # estimated value of the current state
+        M_A: RealNumber,           # current anger/frustration
+        M_S: RealNumber,           # current sadness
+        lambda_A: RealNumber,
+        lambda_C: RealNumber,
+        midpoint: int,
+        eta: RealNumber,
+        gamma: RealNumber,
+        alpha: RealNumber,
+        kappa: RealNumber,
+        w_v_A: RealNumber,
+        I: RealNumber,
+        num_agents=1,
+    ):
+        super().__init__()
+
+        self.num_agents_ = num_agents
+
+        # Override default RNG to numpy (supports multinomial distribution)
+        self.random = np.random.default_rng(seed)
+
+        # Instantiate DataCollector
+        self.datacollector = mesa.DataCollector(
+            model_reporters=None, agent_reporters={
+                **{
+                    name: (lambda agent, key=name: agent._variables[key])
+                    for name in IrritabilityAgent._variable_names
+                }
+            }
+        )
+
+        # Create agents
+        IrritabilityAgent.create_agents(
+            model=self,
+            n=1,  # number of agents
+            V=V,
+            M_A=M_A,
+            M_S=M_S,
+            lambda_A=lambda_A,
+            lambda_C=lambda_C,
+            midpoint=midpoint,
+            eta=eta,
+            gamma=gamma,
+            alpha=alpha,
+            kappa=kappa,
+            w_v_A=w_v_A,
+            I=I,
+        )
+
+    def step(self):
+        self.agents.shuffle_do("act")
+
+        self.datacollector.collect(self)
+
+        self.agents.shuffle_do("update_emotions_and_learn")
+
+
+if __name__ == "__main__":
+    pass
